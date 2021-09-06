c7ba6d08f33ff9fd1c0a9ff28c6de2e99e5e7457
Manish7690 <70003759+Manish7690@users.noreply.github.com>
2021-09-07 01:27:13 +0530

    Update agent_nodes.pp

--- site/profile/menifests/agent_nodes.pp
+++ site/profile/menifests/agent_nodes.pp
@@ -3,3 +3,14 @@ class profile::agent_nodes {
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
 }
+class { 'docker':
+  use_upstream_package_source => false,
+}
+class { 'docker':
+  version => '17.09.0~ce-0~debian',
+}
+class { 'docker':
+  docker_ee => true,
+  docker_ee_source_location => 'https://<docker_ee_repo_url>',
+  docker_ee_key_source => 'https://<docker_ee_key_source_url>',
+}
