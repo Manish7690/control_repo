1776b037df96fc8cd69dd11f2a3f6de902db00d0
Manish7690 <70003759+Manish7690@users.noreply.github.com>
2021-09-08 16:23:33 +0530

    Update ssh_server.pp

--- site/profile/menifests/ssh_server.pp
+++ site/profile/menifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCycayOI87f2Dp5brcmawajNJNvfvsXYogHYbv7w4QNUBLfCQUJCy9Su0lJDFO1oXWm9bdgUrs6Az6zVQkZXkDqtojAqUFQQ2F9f76fMISYxhrjqzHtaI0+e8hnh0bO/Z+8R1ADgVR6wtuPALL/GxZgX9M3ObROn+l8kxam7jseusiFYcHQtoTxEE15l+GqUZCK/6iuWlvO0qTXeEe2wYABIrJ6mBF3BU6cgU0ORWMtVJaWUroOZdQcgSz/VOwLVSDZX/wEGKYtEuV0kr12Izl5cDc0U48CbSfgABE62Lw9GATcxh/jlES8xcXCbt9u87qEIErW0pI1a4bx5RoUqdfJ',
   }  
 }
